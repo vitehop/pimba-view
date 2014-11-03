db63a07d1bfaac7499f829f8e0fdab359be29978
vitehop <victorperez@MacBook-Pro-de-Victor-2.local>
2014-11-03 19:41:24 +0100

    Modal finish

--- js/app.js
+++ js/app.js
@@ -1,19 +1,79 @@
 $(document).ready(function(){
 
-    var resizingTextareas = [].slice.call(document.querySelectorAll('textarea'));
+    $('.description-savecancel-buttons').hide();
+    $('.title-savecancel-buttons').hide();
 
-    resizingTextareas.forEach(function(textarea) {
-        textarea.addEventListener('input', autoresize, false);
-        textarea.addEventListener('focus', autoresize, false);
+    // *****************
+    // EDIT WINDOW BINDS
+    // *****************
+
+    // Show edit controls when clicking on the description field
+
+    $('#modal-description').on('click',function(){
+
+
+        $('#modal-description').summernote({
+            focus:true,
+
+            toolbar: [
+                ['style', ['bold', 'italic', 'underline','strikethrough']],
+                ['para', ['ul', 'ol', 'paragraph']],
+            ]
+        });
+
+        var previousText = $('#modal-description').code();
+
+        $('#modal-description').addClass('description-editor');
+
+        $('.description-savecancel-buttons').show();
+
+        $('.description-cancel').on('click',function(){
+
+            $('#modal-description').code(previousText);
+            $('#modal-description').destroy();
+            $('.description-savecancel-buttons').hide();
+        });
+
+        $('.description-save').on('click',function(){
+
+            $('#modal-description').destroy();
+            $('.description-savecancel-buttons').hide();
+        });
 
     });
 
-    function autoresize() {
-        this.style.height = 'auto';
-        this.style.height = this.scrollHeight+'px';
-        this.scrollTop = this.scrollHeight;
-        window.scrollTo(window.scrollLeft,(this.scrollTop+this.scrollHeight));
-    }
+    // Show edit controls when clicking on the title field
+
+    $('#modal-title').on('click',function(){
+
+        var previousText = $('#modal-title').code();
+
+        $('#modal-title').attr('contenteditable','true');
+        $('#modal-title').addClass('title-editor');
+
+        $('.title-savecancel-buttons').show();
+
+        $('.title-cancel').on('click',function(){
+
+            $('#modal-title').html(previousText);
+            $('#modal-title').attr('contenteditable','false');
+            $('.title-savecancel-buttons').hide();
+            $('#modal-title').removeClass('title-editor');
+
+        });
+
+        $('.title-save').on('click',function(){
+
+            $('#modal-title').attr('contenteditable','false');
+            $('.title-savecancel-buttons').hide();
+            $('#modal-title').removeClass('title-editor');
+
+        });
+
+    });
+
+
+
 
 });
 
